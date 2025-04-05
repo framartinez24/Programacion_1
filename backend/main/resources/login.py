d1fb6a1139b792f69a20713a0b454cac477a8973
LucianoFarazUM <l.faraz@alumno.um.edu.ar>
2025-04-05 20:52:54 -0300

    add usuarios fake in login

--- backend/main/resources/login.py
+++ backend/main/resources/login.py
@@ -1,6 +1,12 @@
 from flask_restful import Resource
 from flask import request
 
+USUARIOS = {
+    1 : {"nombre" : "Juan", "correo" : "Juan@gmail.com", "direccion" : "Calle 123", "Contraseña" : "12345", "Telefono" : 2611234567, "Rol" : "Administrador"},
+    2 : {"nombre" : "Jose", "correo" : "Jose@gmail.com", "direccion" : "Calle 456", "Contraseña" : "12345", "Telefono" : 2611234568, "Rol" : "Usuario"},
+    3 : {"nombre" : "Pepe", "correo" : "Pepe@gmail.com", "direccion" : "Calle 112", "Contraseña" : "12345", "Telefono" : 2611234569, "Rol" : "Encargado"}
+}
+
 class LoginRecurso(Resource):
     def post(self):
         data = request.get_json()
