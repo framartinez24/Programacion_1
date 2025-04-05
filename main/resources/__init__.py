40513c15528168492304a30a822d261fafb0fe2e
Niqoo10 <nicopanella2009@hotmail.com>
2025-04-05 19:22:17 -0300

    Arreglado

--- main/resources/__init__.py
+++
@@ -1,7 +0,0 @@
-from .usuario import UsuarioRecurso, UsuariosRecursos
-from .productos import ProductoRecurso, ProductosRecursos
-from .login import LoginRecurso
-from .logout import LogoutRecurso
-from .pedidos import PedidoRecurso, PedidosRecursos
-from .notificaciones import NotificacionRecurso
-from .valoracion import ValoracionRecurso
--- main/resources/logout.py
+++
@@ -1,6 +0,0 @@
-from flask_restful import Resource
-from flask import request
-
-class LogoutRecurso(Resource):
-    def post(self):
-        return {"mensaje": "Sesión cerrada correctamente"}, 200
